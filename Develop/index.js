2e1c1c8dbb6051e690dd31563733d5581162c021
Osgood <mosgood88@gmail.com>
2025-03-13 20:58:52 -0400

    add application

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,138 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a short description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license will you use?',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    const content = `
+# ${data.title}
 
-// TODO: Create a function to initialize app
-function init() {}
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contribution
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+## License
+
+${renderLicenseSection(data.license)}
+
+## Questions
+
+If you have any questions, please contact me at [${data.email}](mailto:${data.email}). 
+You can also find my GitHub profile here [${data.github}](https://github.com/${data.github}).
+    `;
+
+    fs.writeFile(fileName, content, (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+        } else {
+            console.log('README file created successfully!');
+        }
+    });
+}
+
+// Function to render license badge
+function renderLicenseBadge(license) {
+    if (license === 'None') {
+        return '';
+    }
+    return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+}
+
+// Function to render license section
+function renderLicenseSection(license) {
+    if (license === 'None') {
+        return 'This project is not licensed.';
+    }
+    return `This project is licensed under the ${license} license.`;
+}
+
+// Function to initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            writeToFile('README.md', answers);
+        })
+        .catch((error) => {
+            console.error('Error initializing app:', error);
+        });
+}
 
 // Function call to initialize app
 init();
